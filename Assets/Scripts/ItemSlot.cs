e6aa0272b2b72c0559b4a8d329dca9406a552d13
junhyungs <kojy2004@yonsei.ac.kr>
2024-05-10 13:47:59 +0900

    Invetory 커밋
    Dictionary 작업중

---
+++ Assets/Scripts/ItemSlot.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSlot : MonoBehaviour
+{
+    public void UpdateSlot(Item item)
+    {
+
+    }
+}
--- Assets/Scripts/Managers/ItemDatabase.cs
+++ Assets/Scripts/Managers/ItemDatabase.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemDatabase : MonoBehaviour
+public class ItemDatabase : Singleton<ItemDatabase>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    Dictionary<ItemName, Item> itemDictionary = new Dictionary<ItemName, Item>();
+    Dictionary<ItemName, Sprite> itemSpriteDictionary = new Dictionary<ItemName, Sprite>();
+    private void Awake()
+    {        
+        itemDictionary.Add(ItemName.ClayMore1, new Item(ItemName.ClayMore1, 1, true, 70, DefenceType.Head, 0));
+        itemDictionary.Add(ItemName.ClayMore2, new Item(ItemName.ClayMore2, 1, true, 60, DefenceType.Head, 0));
     }
 
-    // Update is called once per frame
-    void Update()
+    public Item GetItem(ItemName itemName)
     {
-        
+        return itemDictionary[itemName];
+    }
+
+    public Sprite GetSprite(ItemName itemName)
+    {
+        return itemSpriteDictionary[itemName];
     }
 }
