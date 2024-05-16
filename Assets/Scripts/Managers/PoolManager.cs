c2b9cd52659261ea4342e0c78820874c4bb0a66d
재연 <kojy2004@yonsei.ac.kr>
2024-05-16 15:32:10 +0900

    Update PoolManager.cs

--- Assets/Scripts/Managers/PoolManager.cs
+++ Assets/Scripts/Managers/PoolManager.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PoolManager : Singleton<PoolManager>
 {
     public GameObject itemSlotPrefab;
+    public GameObject getSlotPrefab;
     public GameObject itemDropPrefab;
     public GameObject elementPrefab;
 
     Stack<GameObject> itemSlotStack;
+    Stack<GameObject> getSlotStack;
     Stack<GameObject> itemDropStack;
     Queue<GameObject> elementQueue;
 
@@ -19,17 +21,35 @@ public class PoolManager : Singleton<PoolManager>
     private void Awake()
     {
         itemSlotStack = new Stack<GameObject>();
+        getSlotStack = new Stack<GameObject>();
         elementQueue = new Queue<GameObject>();
 
-        for(int i = 0; i < 200; i++)
+        for (int i = 0; i < 200; i++)
         {
-            GameObject prefab = Instantiate(itemSlotPrefab,PoolParent.transform);
-            prefab.name = i.ToString();
+            GameObject prefab = Instantiate(itemSlotPrefab, PoolParent.transform);
+            prefab.name = $"itemSlot[{i}]";
             itemSlotStack.Push(prefab);
             prefab.SetActive(false);
         }
 
-        for(int i = 0; i < 100; i++)
+        for (int i = 0; i < 30; i++)
+        {
+            GameObject prefab = Instantiate(getSlotPrefab, PoolParent.transform);
+            prefab.name = $"getSlot[{i}]";
+            getSlotStack.Push(prefab);
+            prefab.SetActive(false);
+        }
+
+        for (int i = 0; i < 30; i++)
+        {
+            GameObject prefab = Instantiate(itemDropPrefab, PoolParent.transform);
+            prefab.name = $"itemDrop[{i}]";
+            itemDropStack.Push(prefab);
+            prefab.GetComponent<DropObject>().SetId(i);
+            prefab.SetActive(false);
+        }
+
+        for (int i = 0; i < 100; i++)
         {
             GameObject prefab = Instantiate(elementPrefab, ElementPool);
             ElementObject element = prefab.GetComponent<ElementObject>();
@@ -38,11 +58,11 @@ public class PoolManager : Singleton<PoolManager>
             elementQueue.Enqueue(prefab);
         }
     }
-    public ItemSlot GetItemSlot()
+    public ItemSlot Get_ItemSlot()
     {
         ItemSlot itemSlot;
         GameObject prefab;
-        if(itemSlotStack.TryPop(out prefab))
+        if (itemSlotStack.TryPop(out prefab))
         {
             itemSlot = prefab.GetComponent<ItemSlot>();
             return itemSlot;
@@ -60,5 +80,44 @@ public class PoolManager : Singleton<PoolManager>
         GameObject prefab = elementQueue.Dequeue();
         elementQueue.Enqueue(prefab);
         return prefab;
+    }
+
+    public GetSlot Get_GetSlot()
+    {
+        GetSlot getSlot;
+        GameObject prefab;
+        if (getSlotStack.TryPop(out prefab))
+        {
+            getSlot = prefab.GetComponent<GetSlot>();
+            return getSlot;
+        }
+        else
+        {
+            prefab = Instantiate(getSlotPrefab, PoolParent.transform);
+            getSlot = prefab.GetComponent<GetSlot>();
+            return getSlot;
+        }
+    }
+
+    public void Return_itemSlot(ItemSlot itemSlot)
+    {
+        itemSlotStack.Push(itemSlot.gameObject);
+        itemSlot.gameObject.SetActive(false);
+        itemSlot.transform.SetParent(PoolParent);
+    }
+
+
+    public void Return_GetSlot(GetSlot getSlot)
+    {
+        getSlotStack.Push(getSlot.gameObject);
+        getSlot.gameObject.SetActive(false);
+        getSlot.transform.SetParent(PoolParent);
+    }
+
+    public void Return_itemDrop(GameObject itemDrop)
+    {
+        itemDropStack.Push(itemDrop);
+        itemDrop.SetActive(false);
+        itemDrop.transform.SetParent(PoolParent);
     }
 }
