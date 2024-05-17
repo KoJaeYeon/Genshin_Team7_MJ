e7c5e74aa6591856f399f9c1218af6208ac3abf9
재연 <kojy2004@yonsei.ac.kr>
2024-05-17 11:10:38 +0900

    UI수정

---
+++ Assets/Scripts/UI/characterItemSettingButton.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class characterSettingButton : MonoBehaviour
+{
+    public Transform TextPanel_Parent;
+    GameObject[] TextPanel;
+    public Transform select_Parent;
+    GameObject selectAnimateImage;
+    GameObject[] select_Panel_Icon_Image;
+    private void Awake()
+    {
+        select_Parent = transform;
+        TextPanel = new GameObject[3];
+        select_Panel_Icon_Image = new GameObject[3];
+        selectAnimateImage = select_Parent.transform.GetChild(3).gameObject;
+        for(int i = 0; i < 3; i++)
+        {
+            TextPanel[i] = TextPanel_Parent.transform.GetChild(i).gameObject;
+            select_Panel_Icon_Image[i] = select_Parent.transform.GetChild(i).GetChild(0).gameObject;
+        }
+    }
+
+    private void Start()
+    {
+        SelectActive(0);
+    }
+
+    public void SelectActive(int index)
+    {
+        for (int i = 0; i < TextPanel.Length; i++)
+        {
+            if (i == index)
+            {
+                TextPanel[i].SetActive(true);
+                select_Panel_Icon_Image[i].gameObject.SetActive(false);
+                selectAnimateImage.transform.position = select_Panel_Icon_Image[i].transform.position;
+            }
+            else
+            {
+                TextPanel[i].SetActive(false);
+                select_Panel_Icon_Image[i].gameObject.SetActive(true);
+            }
+        }
+    }
+}
