59779e5bcfaefe7a92eb877dcf60660e7ae9f37b
심준형 <jin05003@naver.com>
2024-05-23 10:23:56 +0900

    [Update]
    대미지 텍스트 추가

--- Assets/Scripts/Enemy/Monster/FireHilichurl.cs
+++ Assets/Scripts/Enemy/Monster/FireHilichurl.cs
@@ -48,18 +48,6 @@ public class FireHilichurl : Enemy,IColor
         return color;
     }
 
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.gameObject.CompareTag("HitObject") && gameObject.layer == (int)EnemyLayer.isAlive)
-    //    {
-    //        TestElement hitObject = other.GetComponent<TestElement>();
-    //        HitElement = hitObject.GetElement();
-    //        Damaged(this, hitObject.ReturnDamage(), HitElement);
-
-    //        hitObject.Return();
-    //    }
-    //}
-
     public override void TakeDamage(float damage, Element element)
     {
         EnemyHealthDic[this] -= CalculateDamage(damage, element);
